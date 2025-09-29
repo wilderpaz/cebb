d1c694c5f2672d97b64c3486f70fadac9c2dbbbe
Wilder Paz <44214094+wilderpaz@users.noreply.github.com>
2025-09-28 21:34:03 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
+// Para dificultar o acesso direto, a senha foi codificada em Base64.
+// Este método não é criptografia, mas oculta a senha da visualização casual do código-fonte.
+// Senha original: Entrar em contato com Wilder Paz através do e-mail: wpbcpaz@gmail.com ou fazer leitura da autenticação
+const AUTH_TOKEN = 'Y2ViYjIwMjU=';
 
