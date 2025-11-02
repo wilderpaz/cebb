d42388d856667715eeb40b543240fe81f5187a70
Wilder Paz <44214094+wilderpaz@users.noreply.github.com>
2025-11-02 20:47:33 -0300

    Update config.js
    alteração de codificação para senha de acesso ao dashboard

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 // Para dificultar o acesso direto, a senha foi codificada em Base64.
 // Este método não é criptografia, mas oculta a senha da visualização casual do código-fonte.
 // Senha original: Entrar em contato com Wilder Paz através do e-mail: wpbcpaz@gmail.com ou fazer leitura da autenticação
-const AUTH_TOKEN = 'Y2ViYjIwMjU=';
+const AUTH_TOKEN = "$2b$10$fdM1BgjMKlnPlgvhjCcA.evc66zfQ/5nqJMdHsiWMUlhGqX5N9/V2";
+module.exports = { AUTH_TOKEN };
+// config.js
+
+
 
