0ca2e1a7f5edadbbf34dab549ebe50b87caa1989
Wilder Paz <44214094+wilderpaz@users.noreply.github.com>
2025-11-02 20:49:28 -0300

    Update password encoding method to bcrypt

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-// Para dificultar o acesso direto, a senha foi codificada em Base64.
+// Para dificultar o acesso direto, a senha foi codificada em bcrypt.
 // Este método não é criptografia, mas oculta a senha da visualização casual do código-fonte.
 // Senha original: Entrar em contato com Wilder Paz através do e-mail: wpbcpaz@gmail.com ou fazer leitura da autenticação
 const AUTH_TOKEN = "$2b$10$fdM1BgjMKlnPlgvhjCcA.evc66zfQ/5nqJMdHsiWMUlhGqX5N9/V2";
 module.exports = { AUTH_TOKEN };
-// config.js
 
 
 
